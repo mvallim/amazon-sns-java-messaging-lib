3deca3f6705a6ef8ce967551362380e5e4ca00d7
Marcos Tischer Vallim <tischer@gmail.com>
2025-07-27 18:31:04 -0300

    chore: add asl
    Signed-off-by: Marcos Tischer Vallim <tischer@gmail.com>

--- amazon-sns-java-messaging-lib-template/src/main/java/com/amazon/sns/messaging/lib/concurrent/ExecutorsProvider.java
+++ amazon-sns-java-messaging-lib-template/src/main/java/com/amazon/sns/messaging/lib/concurrent/ExecutorsProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.amazon.sns.messaging.lib.concurrent;
 
 import java.lang.reflect.Method;
@@ -14,11 +30,11 @@ import lombok.SneakyThrows;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ExecutorsProvider {
-  
+
   private static final Logger LOGGER = LoggerFactory.getLogger(ExecutorsProvider.class);
-  
+
   private static Supplier<ExecutorService> supplierExecutorService;
-  
+
   static {
     if (ExecutorsProvider.getJavaVersion() >= 21) {
       ExecutorsProvider.supplierExecutorService = ExecutorsProvider::getVirtualThreadExecutor;
@@ -28,35 +44,35 @@ public final class ExecutorsProvider {
       ExecutorsProvider.LOGGER.info("Java version is {}, using default thread executor", ExecutorsProvider.getJavaVersion());
     }
   }
-
+  
   public static ExecutorService getThreadExecutor() {
     return ExecutorsProvider.supplierExecutorService.get();
   }
-
+  
   @SneakyThrows
   private static ExecutorService getDefaultThreadExecutor() {
     return Executors.newSingleThreadExecutor();
   }
-
+  
   @SneakyThrows
   private static ExecutorService getVirtualThreadExecutor() {
     final Class<?> clazzThread = Executors.class;
     final Method ofVirtualMethod = clazzThread.getMethod("newVirtualThreadPerTaskExecutor");
     return ExecutorService.class.cast(ofVirtualMethod.invoke(null));
   }
-  
+
   private static int getJavaVersion() {
     String version = System.getProperty("java.version");
-
+    
     if (version.startsWith("1.")) {
       version = version.substring(2);
     }
-
+    
     final int dotPos = version.indexOf('.');
     final int dashPos = version.indexOf('-');
     final int endIndex = dotPos > -1 ? dotPos : dashPos > -1 ? dashPos : 1;
-
+    
     return Integer.parseInt(version.substring(0, endIndex));
   }
-
+  
 }
--- amazon-sns-java-messaging-lib-template/src/main/java/com/amazon/sns/messaging/lib/instrument/RingBufferBlockingQueueJmx.java
+++ amazon-sns-java-messaging-lib-template/src/main/java/com/amazon/sns/messaging/lib/instrument/RingBufferBlockingQueueJmx.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.amazon.sns.messaging.lib.instrument;
 
 import com.amazon.sns.messaging.lib.concurrent.RingBufferBlockingQueue;
@@ -6,37 +22,37 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class RingBufferBlockingQueueJmx implements RingBufferBlockingQueueJmxMBean {
-
+  
   private final RingBufferBlockingQueue<?> ringBufferBlockingQueue;
-
+  
   @Override
   public int capacity() {
-    return this.ringBufferBlockingQueue.capacity();
+    return ringBufferBlockingQueue.capacity();
   }
-
+  
   @Override
   public int size() {
-    return this.ringBufferBlockingQueue.size();
+    return ringBufferBlockingQueue.size();
   }
-
+  
   @Override
   public boolean isEmpty() {
-    return this.ringBufferBlockingQueue.isEmpty();
+    return ringBufferBlockingQueue.isEmpty();
   }
-
+  
   @Override
   public boolean isFull() {
-    return this.ringBufferBlockingQueue.isFull();
+    return ringBufferBlockingQueue.isFull();
   }
-
+  
   @Override
   public long writeSequence() {
-    return this.ringBufferBlockingQueue.writeSequence();
+    return ringBufferBlockingQueue.writeSequence();
   }
-
+  
   @Override
   public long readSequence() {
-    return this.ringBufferBlockingQueue.readSequence();
+    return ringBufferBlockingQueue.readSequence();
   }
-
+  
 }
